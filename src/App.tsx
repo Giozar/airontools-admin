bdb6f8deadd9da309e1e9216e48f2b281d7e60b2
Citlalli <citlalli688@outlook.com>
2024-07-15 13:34:17 -0600

    feat: implementacion de react-router para inicio manejar la navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,45 @@
 import { useState, createContext} from 'react'
 import { UserDataFrontend} from './adapter';
-
-import aironLogo from '/vite.svg'
-import './App.css'
-
-//Se importan las rutas de los componentes
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Home from './Bienvenida';
 import React from 'react';
 
-//Esta interfaz se encarga de guardar el usuario y decir si está logeado o no
+
 interface AuthContextType {
   isAuthenticated: boolean;
   user: UserDataFrontend | null;
   setAuth: (auth: { isAuthenticated: boolean; user: UserDataFrontend | null }) => void;
 }
-//Crea el contexto de la aplicación, funciona como una variable global
-//Que guarda el usuario si esta logeado o no
+
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-//Es donde está el titulo de la página, esto se puede cambiar de lugar o cambiar
-function Header(){
-  return(
-    <header>
-      <img src={aironLogo} alt='logo de airon tools'></img>
-      <h1>Administrador de Herramientas AironTools</h1>
-    </header>
-  );
-}
-
 function App() {
-  //El estado inicial es que no hay usuario
   const [auth, setAuth] = useState<{ isAuthenticated: boolean; user: UserDataFrontend | null }>({
     isAuthenticated: false,
     user: null,
   });
-  //Por ahora solo muestra login o home, esto se puede cambiar en un futuro
+
   return (
     <>
-     <AuthContext.Provider value={{ ... auth, setAuth }}>
-        <Header/>
-        <PrivateRoute Component={Home}/>
-      </AuthContext.Provider>
+    <AuthContext.Provider value={{ ... auth, setAuth }}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route element={<PrivateRoute/>}>
+          <Route path="/home" element={<Home/>}/>  
+        </Route>
+      </Routes>
+      </BrowserRouter>  
+    </AuthContext.Provider>
     </>
   );
 }
-//Esta funcion puede ser redundante pero es para mostrar login o home dependiendo
-function PrivateRoute({ Component }: { Component: React.ComponentType }) {
+
+const PrivateRoute = ()=>{
   const authContext = React.useContext(AuthContext);
-
   if (!authContext) return null;
-
-  return authContext.isAuthenticated ? <Component /> : <Login/>;
+  return authContext.isAuthenticated ? < Outlet/> : <Navigate to="/login"/>;
 }
 
 export default App
