73872a18ec4450c48e781edda8d655825765b0f8
Citlalli <citlalli688@outlook.com>
2024-07-17 15:52:02 -0600

    feat: ahora se puede cerrar sesión

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -2,11 +2,24 @@
 TODO: que tenga un css consistente durante todas las paginas
 */
 
+import { Link, useNavigate } from 'react-router-dom';
 import ComboBox from '../components/ComboBox';
 import aironLogo from '/vite.svg';
+import { AuthContext } from '../App';
+import { useContext } from 'react';
 
 
 function Sidebar(){
+    const navigate = useNavigate();
+    const authContext = useContext(AuthContext);
+    
+    const handleOptionSelected = (selectedOption: string) => {
+        console.log("opcion seleccionada:", selectedOption);
+      };
+    const handleClose = () => {
+        authContext?.setAuth({ isAuthenticated: false, user: null });
+        navigate('/login');
+    }
     return (
         <div className='sidebar'>
             <div className='top'>
@@ -22,19 +35,24 @@ function Sidebar(){
                     Notificaciones
                 </a>
                 <ComboBox option='Herramientas' 
-                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Herramientas')}/>
+                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Herramientas')}
+                onOptionSelected={handleOptionSelected}
+                />
                 <ComboBox option='Empleados'
-                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Empleados')} />
+                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Empleados')} 
+                onOptionSelected={handleOptionSelected}/>
                 <ComboBox option='Especificaciones'
-                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Especificaciones')} />
+                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Especificaciones')}
+                onOptionSelected={handleOptionSelected} />
                 <ComboBox option='Roles' 
-                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Roles')}/>
+                options = {["Ver", "Crear", "Actualizar", "Eliminar"].map((val)=>val+' Roles')}
+                onOptionSelected={handleOptionSelected}/>
             </div>
             <div className="bottom">
                 <nav>
                     <a href="#">Información personal</a>
                     <a href="#">Seguridad</a>
-                    <a href="#">Cerrar sesión</a>
+                    <Link to="/login" onClick={handleClose}>Cerrar sesión</Link>
                 </nav>
             </div>
         </div>
