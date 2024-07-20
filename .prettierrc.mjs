ef59613fe87f3119d7fbffd9eb5e3a0d3e68bc4a
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-07-20 02:40:02 -0600

    chore: Instalación de dependencias y configuración de eslint y prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+	singleQuote: true,
+	semi: true,
+	printWidth: 80,
+	tabWidth: 2,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+    jsxSingleQuote: true,
+    endOfLine: 'auto',
+};
