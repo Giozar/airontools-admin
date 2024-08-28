529e745ce74a53e7241f3c07cd0836927b868782
Citlalli <citlalli688@outlook.com>
2024-08-28 08:33:53 -0600

    chore: cambio de nombre de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,7 +25,7 @@ function HeaderLogin() {
 	return (
 		<header>
 			<img src={aironLogo} alt='logo de airon tools' className='logoimg' />
-			<h1>Administrador de Herramientas AironTools</h1>
+			<h1>¡Bienvenido a tu sistema de gestión de trabajo AironTools!</h1>
 		</header>
 	);
 }
