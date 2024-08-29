713db2592e66c7634f9630527e19311a8c6fb2e7
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-08-29 16:39:01 -0600

    test: Prueba de ruta de login sin validación

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,3 +1,2 @@
 // puerto de comicación entre backend y frontend para cors
-export const airontoolsAPI =
-	import.meta.env.VITE_API_URL || 'http://192.168.10.115:4000';
+export const airontoolsAPI = import.meta.env.VITE_API_URL; // || 'http://192.168.10.115:4000';
