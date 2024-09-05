70d542156646b3743f90d0888be48418ac420b42
Citlalli <citlalli688@outlook.com>
2024-09-05 16:21:17 -0600

    add: boton para cerrar la edicion del rol de un usuario

--- src/components/roles/RoleChangeModal.tsx
+++ src/components/roles/RoleChangeModal.tsx
@@ -6,6 +6,7 @@ import { RegisterResponse, UserDataFrontend } from '@interfaces/User.interface';
 import axios from 'axios';
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
+import CloseIcon from '@components/svg/CloseIcon';
 import { airontoolsAPI } from '@configs/api.config';
 import '../css/roleChangeModal.css';
 
@@ -67,25 +68,22 @@ function RoleChangeModal({
 	};
 
 	return (
-		<>
+		<form onSubmit={handleSubmit} className='choserol'>
 			{errorLog.isError && <ErrorMessage message={errorLog.message} />}
-			<form onSubmit={handleSubmit} className='choserol'>
-				<p>Nuevo rol para: {userToEdit?.name}</p>
-				<label htmlFor='options'>Nuevo rol:</label>
-				<select
-					id='options'
-					value={role as string}
-					onChange={handleOptionChange}
-				>
-					{roleOptions.map((roleOption: RoleDataFront, index) => (
-						<option key={index} value={roleOption.id}>
-							{roleOption.name}
-						</option>
-					))}
-				</select>
-				<button type='submit'>Cambiar</button>
-			</form>
-		</>
+			<button type='button' className='close' onClick={onCloseModal}>
+				<CloseIcon />
+			</button>
+			<p>Nuevo rol para: {userToEdit?.name}</p>
+			<label htmlFor='options'>Nuevo rol:</label>
+			<select id='options' value={role as string} onChange={handleOptionChange}>
+				{roleOptions.map((roleOption: RoleDataFront, index) => (
+					<option key={index} value={roleOption.id}>
+						{roleOption.name}
+					</option>
+				))}
+			</select>
+			<button type='submit'>Cambiar</button>
+		</form>
 	);
 }
 export default RoleChangeModal;
