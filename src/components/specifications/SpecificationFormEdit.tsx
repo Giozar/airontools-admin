8b9882819cb5b78df8972581d945a2863c9ea281
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-09-05 12:22:06 -0600

    feat: Se implementa edición de especificaciones funcional

--- src/components/specifications/SpecificationFormEdit.tsx
+++ src/components/specifications/SpecificationFormEdit.tsx
@@ -6,7 +6,7 @@ import useErrorHandling from '@hooks/common/useErrorHandling';
 import useSuccessHandling from '@hooks/common/useSuccessHandling';
 import { SpecificationFormEditProps } from '@interfaces/SpecificationFormProps.interface';
 import { SpecDataToSend } from '@interfaces/Specifications.interface';
-import createSpecification from '@services/specifications/createSpecification.service';
+import editSpecification from '@services/specifications/editSpecification.service';
 import { errorHandler } from '@utils/errorHandler.util';
 import { useContext, useEffect, useState } from 'react';
 
@@ -16,132 +16,80 @@ function SpecificationFormEdit({
 	categoriesId,
 	subcategoriesId,
 }: SpecificationFormEditProps) {
-	const [specification, setSpecification] = useState<SpecDataToSend>(
-		specToEdit as SpecDataToSend,
-	);
+	const id = specToEdit?._id as string;
+	const [specification, setSpecification] = useState<SpecDataToSend>({
+		name: specToEdit?.name || '',
+		description: specToEdit?.description || '',
+		unit: specToEdit?.unit || '',
+		createdBy: '',
+		families: familiesId,
+		categories: categoriesId,
+		subcategories: subcategoriesId || '',
+	});
+
 	const { showError, errorLog } = useErrorHandling();
+	const { showSuccess, successLog } = useSuccessHandling();
 	const authContext = useContext(AuthContext);
 	const createdBy = authContext?.user?.id || 'user';
-	const { showSuccess, successLog } = useSuccessHandling();
 
-	// Inicializa el estado de especificaciones cada vez que cambian las IDs
 	useEffect(() => {
-		setSpecification([
-			{
-				name: '',
-				description: '',
-				unit: '',
-				createdBy,
-				families: familiesId,
-				categories: categoriesId,
-				subcategories: subcategoriesId || '',
-			},
-		]);
+		// Actualiza el estado de la especificación cuando cambian las IDs o el usuario autenticado
+		setSpecification(prevSpec => ({
+			...prevSpec,
+			createdBy,
+			families: familiesId,
+			categories: categoriesId,
+			subcategories: subcategoriesId || '',
+		}));
 	}, [familiesId, categoriesId, subcategoriesId, createdBy]);
 
-	// Añade una nueva especificación
-	const addSpecifications = () => {
-		setSpecifications(prevSpecs => [
-			...prevSpecs,
-			{
-				name: '',
-				description: '',
-				unit: '',
-				createdBy,
-				families: familiesId,
-				categories: categoriesId,
-				subcategories: subcategoriesId || '',
-			},
-		]);
+	// Maneja cambios en los campos de la especificación
+	const handleInputChange = (field: keyof SpecDataToSend, value: string) => {
+		setSpecification(prevSpec => ({
+			...prevSpec,
+			[field]: value,
+		}));
 	};
 
-	// Elimina una especificación por su índice
-	const deleteSpecification = (index: number) => {
-		setSpecifications(prevSpecs => prevSpecs.filter((_, i) => i !== index));
-	};
-
-	// Maneja cambios en los campos de una especificación
-	const handleInputChangeInSpec = (
-		index: number,
-		field: keyof SpecDataToSend,
-		value: string,
-	) => {
-		setSpecifications(prevSpecs => {
-			const newSpecs = [...prevSpecs];
-			newSpecs[index] = {
-				...newSpecs[index],
-				[field]: value,
-			};
-			return newSpecs;
-		});
-	};
-
-	// Guarda las especificaciones
-	const saveSpecifications = async () => {
-		for (const specification of specifications) {
-			try {
-				await createSpecification({
-					specification,
-				});
-				showSuccess('Especificación creada con éxito');
-			} catch (error) {
-				errorHandler(error, showError);
-			}
+	// Guarda la especificación
+	const saveSpecification = async () => {
+		try {
+			await editSpecification({ specification, id });
+			showSuccess('Especificación creada con éxito');
+		} catch (error) {
+			errorHandler(error, showError);
 		}
 	};
 
 	return (
 		<div id='specifications'>
-			{<ErrorMessage message={errorLog.message} />}
-			{<SuccessMessage message={successLog.message} />}
-			{specifications.map((spec, index) => (
-				<div
-					key={index}
-					id={`especificacion-${index}`}
-					className='especificacion'
-				>
-					<p>
-						Especificación {index + 1}{' '}
-						<button
-							onClick={() => deleteSpecification(index)}
-							className='delete'
-						>
-							Borrar
-						</button>
-					</p>
+			<ErrorMessage message={errorLog.message} />
+			<SuccessMessage message={successLog.message} />
 
-					<form className='form-group'>
-						<input
-							type='text'
-							placeholder='Nombre'
-							value={spec.name}
-							onChange={e =>
-								handleInputChangeInSpec(index, 'name', e.target.value)
-							}
-							required
-						/>
-						<textarea
-							placeholder='Descripción (opcional)'
-							value={spec.description}
-							onChange={e =>
-								handleInputChangeInSpec(index, 'description', e.target.value)
-							}
-						/>
-						<input
-							id='unit'
-							type='text'
-							placeholder='Unidades (opcional)'
-							value={spec.unit}
-							onChange={e =>
-								handleInputChangeInSpec(index, 'unit', e.target.value)
-							}
-						/>
-					</form>
-				</div>
-			))}
-			<button onClick={addSpecifications}>Agregar Especificación</button>
-			<button onClick={saveSpecifications} className='save'>
-				Guardar Especificaciones
+			<form className='form-group'>
+				<input
+					type='text'
+					placeholder='Nombre'
+					value={specification.name}
+					onChange={e => handleInputChange('name', e.target.value)}
+					required
+				/>
+				<textarea
+					placeholder='Descripción (opcional)'
+					value={specification.description}
+					onChange={e => handleInputChange('description', e.target.value)}
+				/>
+				<input
+					id='unit'
+					type='text'
+					placeholder='Unidades (opcional)'
+					value={specification.unit}
+					onChange={e => handleInputChange('unit', e.target.value)}
+				/>
+			</form>
+
+			<button onClick={saveSpecification} className='save'>
+				Guardar Especificación
 			</button>
 		</div>
 	);
