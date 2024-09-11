67da167dbc3d88eac25e3f515a96a95289deb62d
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-09-11 13:36:26 -0600

    feat: Se implementa componente especial para la carga de archivos

---
+++ src/components/commons/FileInput.tsx
@@ -0,0 +1,97 @@
+import '@components/css/FileInput.css';
+import TrashIcon from '@components/svg/TrashIcon';
+import { ChangeEvent, SetStateAction, useEffect, useState } from 'react';
+
+export default function FileInput({
+	title,
+	images,
+	setImages,
+}: {
+	title: string;
+	images: { [key: string]: File[] };
+	setImages: (
+		value: SetStateAction<{
+			[key: string]: File[];
+		}>,
+	) => void;
+}) {
+	const [filePreviews, setFilePreviews] = useState<{ [key: string]: string[] }>(
+		{},
+	);
+	const [fileNames, setFileNames] = useState<{ [key: string]: string[] }>({});
+	const handleFileSelect = (
+		event: ChangeEvent<HTMLInputElement>,
+		fileType: string,
+	) => {
+		if (event.target.files && event.target.files.length > 0) {
+			const selectedFiles: File[] = Array.from(event.target.files);
+			const previews = selectedFiles.map(file => URL.createObjectURL(file));
+			const names = selectedFiles.map(file => file.name);
+
+			setImages(prev => ({
+				...prev,
+				[fileType]: [...(prev[fileType] || []), ...selectedFiles],
+			}));
+			setFilePreviews(prev => ({
+				...prev,
+				[fileType]: [...(prev[fileType] || []), ...previews],
+			}));
+			setFileNames(prev => ({
+				...prev,
+				[fileType]: [...(prev[fileType] || []), ...names],
+			}));
+		}
+	};
+
+	// Remove a file from the state
+	const handleRemoveFile = (fileType: string, index: number) => {
+		images[fileType] &&
+			setImages(prev => ({
+				...prev,
+				[fileType]: prev[fileType].filter((_, i) => i !== index),
+			}));
+
+		filePreviews[fileType] &&
+			setFilePreviews(prev => ({
+				...prev,
+				[fileType]: prev[fileType].filter((_, i) => i !== index),
+			}));
+		fileNames[fileType] &&
+			setFileNames(prev => ({
+				...prev,
+				[fileType]: prev[fileType].filter((_, i) => i !== index),
+			}));
+	};
+
+	useEffect(() => {}, []);
+
+	return (
+		<div className='image-uploader-container'>
+			<label className='image-uploader-label'>{title}</label>
+			<div className='image-upload'>
+				<div className='image-placeholder add-image'>
+					<label htmlFor='file-input'>{title}</label>
+					<input
+						type='file'
+						id='file-input'
+						multiple
+						accept='image/*'
+						onChange={event => handleFileSelect(event, 'images')}
+					/>
+				</div>
+				{filePreviews.images?.map((preview, index) => (
+					<div key={index} className='image-preview'>
+						<img src={preview} alt={`preview-${index}`} />
+						<button
+							onClick={() => handleRemoveFile('images', index)}
+							className='delete'
+							type='button'
+						>
+							<TrashIcon />
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
--- src/components/commons/ImageUploader.tsx
+++ src/components/commons/ImageUploader.tsx
@@ -1,89 +1,43 @@
 import '@components/css/ImageUploader.css';
 import TrashIcon from '@components/svg/TrashIcon';
-import { ChangeEvent, SetStateAction, useEffect, useState } from 'react';
+import React from 'react';
 
-export default function ImageUploader({
-	title,
-	images,
-	setImages,
-}: {
+interface ImageUploaderProps {
 	title: string;
-	images: { [key: string]: File[] };
-	setImages: (
-		value: SetStateAction<{
-			[key: string]: File[];
-		}>,
+	filePreviews: {
+		[key: string]: string[];
+	};
+	onFileSelect: (
+		event: React.ChangeEvent<HTMLInputElement>,
+		type: string,
 	) => void;
-}) {
-	const [filePreviews, setFilePreviews] = useState<{ [key: string]: string[] }>(
-		{},
-	);
-	const [fileNames, setFileNames] = useState<{ [key: string]: string[] }>({});
-	const handleFileSelect = (
-		event: ChangeEvent<HTMLInputElement>,
-		fileType: string,
-	) => {
-		if (event.target.files && event.target.files.length > 0) {
-			const selectedFiles: File[] = Array.from(event.target.files);
-			const previews = selectedFiles.map(file => URL.createObjectURL(file));
-			const names = selectedFiles.map(file => file.name);
-
-			setImages(prev => ({
-				...prev,
-				[fileType]: [...(prev[fileType] || []), ...selectedFiles],
-			}));
-			setFilePreviews(prev => ({
-				...prev,
-				[fileType]: [...(prev[fileType] || []), ...previews],
-			}));
-			setFileNames(prev => ({
-				...prev,
-				[fileType]: [...(prev[fileType] || []), ...names],
-			}));
-		}
-	};
-
-	// Remove a file from the state
-	const handleRemoveFile = (fileType: string, index: number) => {
-		images[fileType] &&
-			setImages(prev => ({
-				...prev,
-				[fileType]: prev[fileType].filter((_, i) => i !== index),
-			}));
-
-		filePreviews[fileType] &&
-			setFilePreviews(prev => ({
-				...prev,
-				[fileType]: prev[fileType].filter((_, i) => i !== index),
-			}));
-		fileNames[fileType] &&
-			setFileNames(prev => ({
-				...prev,
-				[fileType]: prev[fileType].filter((_, i) => i !== index),
-			}));
-	};
-
-	useEffect(() => {}, []);
-
+	onRemoveFile: (type: string, index: number) => void;
+}
+const ImageUploader: React.FC<ImageUploaderProps> = ({
+	title,
+	filePreviews,
+	onFileSelect,
+	onRemoveFile,
+}) => {
 	return (
 		<div className='image-uploader-container'>
 			<label className='image-uploader-label'>{title}</label>
 			<div className='image-upload'>
 				<div className='image-placeholder add-image'>
-					<label htmlFor='file-input'>{title}</label>
+					<label htmlFor='file-input'>Subir imagen +</label>
 					<input
 						type='file'
 						id='file-input'
 						multiple
 						accept='image/*'
-						onChange={event => handleFileSelect(event, 'images')}
+						onChange={event => onFileSelect(event, 'images')}
 					/>
 				</div>
 				{filePreviews.images?.map((preview, index) => (
 					<div key={index} className='image-preview'>
 						<img src={preview} alt={`preview-${index}`} />
 						<button
-							onClick={() => handleRemoveFile('images', index)}
+							onClick={() => onRemoveFile('images', index)}
 							className='delete'
 							type='button'
 						>
@@ -94,4 +48,6 @@ export default function ImageUploader({
 			</div>
 		</div>
 	);
-}
+};
+
+export default ImageUploader;
