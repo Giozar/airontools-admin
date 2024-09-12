e64d91eb60c0e9a645eaf35d4212c4db6549d24f
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-09-12 02:58:34 -0600

    feat: Se implementa funcionalidad de carga de archivos para imágenes en formulario de productos

--- src/hooks/products/useCreateProduct.ts
+++ src/hooks/products/useCreateProduct.ts
@@ -1,15 +1,23 @@
+import { filesUpload } from '@components/files/helpers/filesUpload.helper';
+import { uploadProductFileUrls } from '@components/products/helpers/UploadProductFileUrls.helper';
 import { useAuthContext } from '@contexts/auth/AuthContext';
 import { useProductCreateContext } from '@contexts/product/ProductContext';
 import createProductService from '@services/products/createProduct.service';
 
-import {
-	uploadProductUrlImages,
-	uploadProductUrlManual,
-} from '@services/products/uploadProductAssets.service';
+import { useEffect } from 'react';
 
 export function useCreateProduct() {
 	const { user } = useAuthContext();
 	const { ...productToCreate } = useProductCreateContext();
+
+	useEffect(() => {
+		productToCreate.id &&
+			uploadProductFileUrls({
+				productId: productToCreate.id,
+				imageUrls: productToCreate.images,
+				manualUrls: productToCreate.manuals,
+			});
+	}, [productToCreate.id, productToCreate.images, productToCreate.manuals]);
 
 	const createProduct = async (e: Event) => {
 		e.preventDefault();
@@ -21,22 +29,19 @@ export function useCreateProduct() {
 			createdBy: user?.id,
 		});
 
-		// Si se creo el producto
-		if (createdProduct) {
-			createdProduct.images &&
-				createdProduct.images.length &&
-				(await uploadProductUrlImages({
-					productId: createdProduct.id,
-					images: createdProduct.images,
-				}));
-
-			createdProduct.manuals &&
-				createdProduct.manuals.length &&
-				(await uploadProductUrlManual({
-					productId: createdProduct.id,
-					manuals: createdProduct.manuals,
-				}));
+		if (createdProduct?.id) {
+			productToCreate.setId(createdProduct.id);
 		}
+
+		productToCreate.imagesRaw.length > 0 &&
+			productToCreate &&
+			(await filesUpload({
+				type: 'images',
+				feature: 'products',
+				files: productToCreate.imagesRaw,
+				setFiles: productToCreate.setImagesRaw,
+				setFileUrls: productToCreate.setImages,
+			}));
 
 		return createdProduct;
 	};
