bd6bb42c2eeb59a42691c225a34cd82cf4db8222
Citlalli <citlalli688@outlook.com>
2024-09-18 10:52:06 -0600

    add: funcionalidad para esconder y ver el header

---
+++ src/components/svg/MenuIcon.tsx
@@ -0,0 +1,34 @@
+interface SVGIconProps {
+	width?: number;
+	height?: number;
+	color?: string;
+	className?: string;
+}
+
+function MenuIcon({
+	width = 24,
+	height = 24,
+	color = 'currentColor',
+	className,
+}: SVGIconProps) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width={width}
+			height={height}
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke={color}
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			className={className}
+		>
+			<line x1='4' x2='20' y1='12' y2='12' />
+			<line x1='4' x2='20' y1='6' y2='6' />
+			<line x1='4' x2='20' y1='18' y2='18' />
+		</svg>
+	);
+}
+
+export default MenuIcon;
--- src/layouts/HeaderApp.tsx
+++ src/layouts/HeaderApp.tsx
@@ -1,25 +1,39 @@
 import Breadcrumb from '@components/commons/Breadcrumb';
+import CircleLeftIcon from '@components/svg/CircleLeftIcon';
+import MenuIcon from '@components/svg/MenuIcon';
 import ThemeToggleButton from '@components/ThemeToggle';
 import { useAuthContext } from '@contexts/auth/AuthContext';
 
-function HeaderApp() {
+interface HeaderAppProps {
+	toggleSidebar: () => void;
+	isSidebarVisible: boolean;
+}
+
+function HeaderApp({ toggleSidebar, isSidebarVisible }: HeaderAppProps) {
 	const { user } = useAuthContext();
 
 	return (
 		<header>
-			<h2>
+			<div className='header__group'>
+				<button
+					onClick={toggleSidebar}
+					className='header__button-toggle-sidebar'
+				>
+					{isSidebarVisible ? <CircleLeftIcon /> : <MenuIcon />}
+				</button>
+				<div className='userinfo'>
+					<div
+						className='userpic'
+						style={{ backgroundImage: `url(${user?.imageUrl})` }}
+					></div>
+					<p>
+						{user?.name} <span>({user?.role?.name})</span>
+					</p>
+					<ThemeToggleButton />
+				</div>
+			</div>
+			<div className='header__group'>
 				<Breadcrumb />
-			</h2>
-
-			<div className='userinfo'>
-				<div
-					className='userpic'
-					style={{ backgroundImage: `url(${user?.imageUrl})` }}
-				></div>
-				<p>
-					{user?.name} <span>({user?.role?.name})</span>
-				</p>
-				<ThemeToggleButton />
 			</div>
 		</header>
 	);
