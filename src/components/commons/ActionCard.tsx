eeaf03dc1bc186e9dbde58f1ee28c50b8fda94ee
Citlalli <citlalli688@outlook.com>
2024-09-18 11:30:17 -0600

    style: un estilo mas consistente para actioncard

--- src/components/commons/ActionCard.tsx
+++ src/components/commons/ActionCard.tsx
@@ -1,13 +1,22 @@
+import '@components/css/ActionsCard.css';
 import FlashIcon from '@components/svg/FlashIcon';
 import { Link } from 'react-router-dom';
+interface ActionCardProps {
+	title: string;
+	path: string;
+	description?: string;
+}
 
-function ActionCard({ title, path }: { title: string; path: string }) {
+function ActionCard({ title, path, description }: ActionCardProps) {
 	return (
-		<Link to={path} className='card'>
-			<dl>
-				<dt>{title}</dt>
-			</dl>
-			<FlashIcon />
+		<Link to={path} className='options__card'>
+			<div className='options__card-content'>
+				<p className='options__card-text'>{title}</p>
+				{description && (
+					<p className='options__card-description'>{description}</p>
+				)}
+			</div>
+			<FlashIcon className='options__card-icon' />
 		</Link>
 	);
 }
