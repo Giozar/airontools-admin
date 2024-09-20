d44959b4a3afa5c887885f8adf1ea81e0d4771fd
Citlalli <citlalli688@outlook.com>
2024-09-20 11:13:22 -0600

    style: mejora de estilo de modalContent

--- src/components/products/ToolInfoModal.tsx
+++ src/components/products/ToolInfoModal.tsx
@@ -1,8 +1,8 @@
 import NumberSelect from '@components/commons/form/NumberSelect';
 import Info from '@components/commons/Info';
 import InfoSection from '@components/commons/InfoSection';
+import ModalContent from '@components/commons/ModalContent';
 import Slideshow from '@components/commons/Slideshow';
-import CloseIcon from '@components/svg/CloseIcon';
 import { airontoolsAPI } from '@configs/api.config';
 import { ProductDataFrontend } from '@interfaces/Product.interface';
 import { useState } from 'react';
@@ -30,104 +30,95 @@ const ToolInfoModal = ({
 	};
 
 	return (
-		<div className='modal-overlay'>
-			<div className='modal-content'>
-				<button className='modal-close' onClick={onClose}>
-					<CloseIcon />
-				</button>
-				{product && product.name && (
-					<div>
-						<h2 style={{ top: 'sticky' }}>
-							{product.name}
-							<div
-								style={{
-									display: 'flex',
-									marginTop: '20px',
-									justifyContent: 'space-around',
-								}}
-							>
-								<NumberSelect
-									size={product.images?.length || 0}
-									title={'Imagen para ficha técnica:'}
-									onSelect={handleNumberSelect}
-								/>
-
-								<button
-									className='add'
-									onClick={() => handlePdfGen(product.id)}
-								>
-									Ver Ficha Técnica (PDF)
-								</button>
-							</div>
-						</h2>
+		<ModalContent isOpen={isOpen} onClose={onClose} title={product?.name || ''}>
+			{product && product.name && (
+				<div>
+					<h2 style={{ top: 'sticky' }}>
 						<div
-							style={{ maxHeight: '60vh', overflowY: 'auto', padding: '20px' }}
+							style={{
+								display: 'flex',
+								marginTop: '20px',
+								justifyContent: 'space-around',
+							}}
 						>
-							<Info title={'Fotos'} info={'-'} />
-							<div className='grupo'>
-								{product.images && (
-									<Slideshow showNumbers={true} images={product.images} />
-								)}
-								<div>
-									<Info title={'Nombre'} info={product.name} />
-									<Info title={'Modelo'} info={product.model} />
-									<Info
-										title={'Familia'}
-										info={product.family ? product.family.name : ''}
-									/>
-									<Info
-										title={'Categoría'}
-										info={product.category ? product.category.name : ''}
-									/>
-									<Info
-										title={'Subcategoría'}
-										info={product.subcategory ? product.subcategory.name : ''}
-									/>
-								</div>
-							</div>
-							<Info title={'Descripción'} info={product.description} />
-							<InfoSection
-								title='Especificaciones'
-								items={
-									product.specifications.map(
-										spec =>
-											`${spec.specification ? spec.specification.name : '**BORRADA**'}: ${spec.value} ${spec.specification ? spec.specification.unit : '**BORRADA**'}`,
-									) || []
-								}
+							<NumberSelect
+								size={product.images?.length || 0}
+								title={'Imagen para ficha técnica:'}
+								onSelect={handleNumberSelect}
 							/>
-							<InfoSection
-								title='Características'
-								items={product.characteristics || []}
-							/>
-							<InfoSection
-								title='Aplicaciones'
-								items={product.applications || []}
-							/>
-							<InfoSection
-								title='Recomendaciones'
-								items={product.recommendations || []}
-							/>
-							<InfoSection
-								title='Requisitos de operación'
-								items={product.operationRequirements || []}
-							/>
-							<InfoSection
-								title='Elementos incluidos'
-								items={product.includedItems || []}
-							/>
-							<InfoSection
-								title='Accesorios opcionales'
-								items={product.optionalAccessories || []}
-							/>
-							<InfoSection title='Videos' items={product.videos || []} />
-							<InfoSection title='Manuales' items={product.manuals || []} />
 
-							<Info title={'Creado por'} info={product.createdBy.name} />
+							<button className='add' onClick={() => handlePdfGen(product.id)}>
+								Ver Ficha Técnica (PDF)
+							</button>
 						</div>
+					</h2>
+					<div
+						style={{ maxHeight: '60vh', overflowY: 'auto', padding: '20px' }}
+					>
+						<Info title={'Fotos'} info={'-'} />
+						<div className='grupo'>
+							{product.images && (
+								<Slideshow showNumbers={true} images={product.images} />
+							)}
+							<div>
+								<Info title={'Nombre'} info={product.name} />
+								<Info title={'Modelo'} info={product.model} />
+								<Info
+									title={'Familia'}
+									info={product.family ? product.family.name : ''}
+								/>
+								<Info
+									title={'Categoría'}
+									info={product.category ? product.category.name : ''}
+								/>
+								<Info
+									title={'Subcategoría'}
+									info={product.subcategory ? product.subcategory.name : ''}
+								/>
+							</div>
+						</div>
+						<Info title={'Descripción'} info={product.description} />
+						<InfoSection
+							title='Especificaciones'
+							items={
+								product.specifications.map(
+									spec =>
+										`${spec.specification ? spec.specification.name : '**BORRADA**'}: ${spec.value} ${spec.specification ? spec.specification.unit : '**BORRADA**'}`,
+								) || []
+							}
+						/>
+						<InfoSection
+							title='Características'
+							items={product.characteristics || []}
+						/>
+						<InfoSection
+							title='Aplicaciones'
+							items={product.applications || []}
+						/>
+						<InfoSection
+							title='Recomendaciones'
+							items={product.recommendations || []}
+						/>
+						<InfoSection
+							title='Requisitos de operación'
+							items={product.operationRequirements || []}
+						/>
+						<InfoSection
+							title='Elementos incluidos'
+							items={product.includedItems || []}
+						/>
+						<InfoSection
+							title='Accesorios opcionales'
+							items={product.optionalAccessories || []}
+						/>
+						<InfoSection title='Videos' items={product.videos || []} />
+						<InfoSection title='Manuales' items={product.manuals || []} />
+
+						<Info title={'Creado por'} info={product.createdBy.name} />
 					</div>
-				)}
-			</div>
-		</div>
+				</div>
+			)}
+		</ModalContent>
 	);
 };
 
