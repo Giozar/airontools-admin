cf1f2b666e194bcc8e1af7c7ee70272b7e3823f5
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-09-27 17:54:54 -0600

    chore: Se cambia endpit en servicios de archivos

--- src/services/files/deleteFile.service.ts
+++ src/services/files/deleteFile.service.ts
@@ -1,8 +1,9 @@
+import { airontoolsAPI } from '@configs/api.config';
 import axios from 'axios';
 
 export const deleteFileService = async (url: string) => {
 	try {
-		const response = await axios.delete(url);
+		const response = await axios.delete(`${airontoolsAPI}/delete-file/${url}`);
 		return response.data;
 	} catch (error) {
 		throw new Error(`No se ha podido eliminar el archivo ${url}: ${error}`);
