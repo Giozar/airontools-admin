3064eb95155389be56b4d9c5dda2dca6f8b62c7d
Citlalli <citlalli688@outlook.com>
2024-10-17 11:43:08 -0600

    chore: separacion de rutas y añadir nuevas

---
+++ src/apps/routes/specificationRoutes.tsx
@@ -0,0 +1,37 @@
+import { SpecificationProvider } from '@contexts/specification/SpecificationContext';
+import CreateSpecification from '@pages/specificationsPages/CreateSpecification';
+import EditSpecification from '@pages/specificationsPages/EditSpecification';
+import ListOfSpecs from '@pages/specificationsPages/ListOfSpecs';
+
+export const specificationRoutes = () => {
+	return [
+		{
+			path: 'especificaciones',
+			element: <ListOfSpecs />,
+		},
+		{
+			path: 'especificaciones',
+			children: createEditSpecificationRoutes(),
+		},
+	];
+};
+export const createEditSpecificationRoutes = () => {
+	return [
+		{
+			path: 'crear-especificaciones',
+			element: (
+				<SpecificationProvider>
+					<CreateSpecification />
+				</SpecificationProvider>
+			),
+		},
+		{
+			path: 'editar-especificacion',
+			element: (
+				<SpecificationProvider>
+					<EditSpecification />
+				</SpecificationProvider>
+			),
+		},
+	];
+};
---
+++ src/apps/routes/toolRoutes.tsx
@@ -0,0 +1,39 @@
+import { ProductCreateProvider } from '@contexts/product/ProductContext';
+import CreateProductPage from '@pages/productPages/CreateProductPage';
+import EditProductPage from '@pages/productPages/EditProductPage';
+import ToolMenu from '@pages/productPages/ProductMenu';
+
+export const toolRoutes = () => {
+	return [
+		{
+			path: 'herramientas',
+			element: <ToolMenu />,
+		},
+
+		{
+			path: 'herramientas',
+
+			children: createEditToolRoutes(),
+		},
+	];
+};
+export const createEditToolRoutes = () => {
+	return [
+		{
+			path: 'crear-herramienta',
+			element: (
+				<ProductCreateProvider>
+					<CreateProductPage />
+				</ProductCreateProvider>
+			),
+		},
+		{
+			path: 'editar-herramienta',
+			element: (
+				<ProductCreateProvider>
+					<EditProductPage />
+				</ProductCreateProvider>
+			),
+		},
+	];
+};
