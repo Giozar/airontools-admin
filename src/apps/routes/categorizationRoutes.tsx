869cfffd8c59ba9114dd807aac26873d6afd03ce
Citlalli <citlalli688@outlook.com>
2024-10-17 12:15:14 -0600

    fix: detalle con las url que no dejaba construir

--- src/apps/routes/categorizationRoutes.tsx
+++ src/apps/routes/categorizationRoutes.tsx
@@ -11,161 +11,147 @@ import EditSubcategoryPage from '@pages/CategorizationPages/Edit/EditSubcategory
 import EditCategorizationRedirect from '@pages/Redirects/EditCategorizationRedirect';
 import { specificationRoutes } from './specificationRoutes';
 
-export const categorizationRoutes = () => {
-	return [
-		{
-			path: 'categorizacion',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<CategorizationMenu />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-		{
-			path: 'categorizacion',
-			children: createEditFamilyRoutes(),
-		},
-		{
-			path: 'categorizacion',
-			children: specificationRoutes(),
-		},
-	];
-};
+export const categorizationRoutes = () => [
+	{
+		path: 'categorizacion',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<CategorizationMenu />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: 'categorizacion',
+		children: specificationRoutes(),
+	},
+	{
+		path: 'categorizacion',
+		children: createEditFamilyRoutes(),
+	},
+];
 
-export const createEditFamilyRoutes = () => {
-	return [
-		{
-			path: 'crear-familia',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<CreateCategorization />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-		{
-			path: 'editar-familia',
-			element: <EditCategorizationRedirect />,
-		},
-		{
-			path: 'editar-familia',
-			children: editFamilyRoutes(),
-		},
-	];
-};
+export const createEditFamilyRoutes = () => [
+	{
+		path: 'crear-familia',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<CreateCategorization />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: 'editar-familia',
+		element: <EditCategorizationRedirect />,
+	},
+	{
+		path: 'editar-familia',
+		children: editFamilyRoutes(),
+	},
+];
 
-export const editFamilyRoutes = () => {
-	return [
-		{
-			path: ':familyId',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<EditCategorization />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-		{
-			path: ':familyId',
-			children: createEditCategoryRoutes(),
-		},
-	];
-};
-export const createEditCategoryRoutes = () => {
-	return [
-		{
-			path: 'crear-categoria',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<CreateCategoryPage />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-		{
-			path: 'editar-categoria',
-			element: <EditCategorizationRedirect />,
-		},
-		{
-			path: 'editar-categoria',
-			children: editCategoryRoutes(),
-		},
-	];
-};
-export const editCategoryRoutes = () => {
-	return [
-		[
-			{
-				path: ':categoryId',
-				element: (
-					<FamilyCreateProvider>
-						<CategoryCreateProvider>
-							<SubcategoryCreateProvider>
-								<EditCategoryPage />
-							</SubcategoryCreateProvider>
-						</CategoryCreateProvider>
-					</FamilyCreateProvider>
-				),
-			},
-			{
-				path: ':categoryId',
-				children: createEditSubcategoryRoutes(),
-			},
-		],
-	];
-};
+export const editFamilyRoutes = () => [
+	{
+		path: ':familyId',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<EditCategorization />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: ':familyId',
+		children: createEditCategoryRoutes(),
+	},
+];
 
-export const createEditSubcategoryRoutes = () => {
-	return [
-		{
-			path: 'crear-subcategoria',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<CreateSubcategoryPage />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-		{
-			path: 'editar-subcategoria',
-			element: <EditCategorizationRedirect />,
-		},
-		{
-			path: 'editar-subcategoria',
-			children: editSubcategoryRoutes(),
-		},
-	];
-};
+export const createEditCategoryRoutes = () => [
+	{
+		path: 'crear-categoria',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<CreateCategoryPage />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: 'editar-categoria',
+		element: <EditCategorizationRedirect />,
+	},
+	{
+		path: 'editar-categoria',
+		children: editCategoryRoutes(),
+	},
+];
 
-export const editSubcategoryRoutes = () => {
-	return [
-		{
-			path: ':subcategoryId',
-			element: (
-				<FamilyCreateProvider>
-					<CategoryCreateProvider>
-						<SubcategoryCreateProvider>
-							<EditSubcategoryPage />
-						</SubcategoryCreateProvider>
-					</CategoryCreateProvider>
-				</FamilyCreateProvider>
-			),
-		},
-	];
-};
+export const editCategoryRoutes = () => [
+	{
+		path: ':categoryId',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<EditCategoryPage />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: ':categoryId',
+		children: createEditSubcategoryRoutes(),
+	},
+];
+
+export const createEditSubcategoryRoutes = () => [
+	{
+		path: 'crear-subcategoria',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<CreateSubcategoryPage />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+	{
+		path: 'editar-subcategoria',
+		element: <EditCategorizationRedirect />,
+	},
+	{
+		path: 'editar-subcategoria',
+		children: editSubcategoryRoutes(),
+	},
+];
+
+export const editSubcategoryRoutes = () => [
+	{
+		path: ':subcategoryId',
+		element: (
+			<FamilyCreateProvider>
+				<CategoryCreateProvider>
+					<SubcategoryCreateProvider>
+						<EditSubcategoryPage />
+					</SubcategoryCreateProvider>
+				</CategoryCreateProvider>
+			</FamilyCreateProvider>
+		),
+	},
+];
