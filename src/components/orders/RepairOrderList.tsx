3286420da505ca75795193f688b959ef52005283
Citlalli <citlalli688@outlook.com>
2024-10-18 13:41:05 -0600

    chore: quitar reverse

--- src/components/orders/RepairOrderList.tsx
+++ src/components/orders/RepairOrderList.tsx
@@ -36,29 +36,27 @@ export default function RepairOrderList() {
 			'PDF',
 			'Editar Orden',
 		],
-		rows: orders
-			.map(order => [
-				`AT${order.control}`,
-				new Date(order.createdAt).toLocaleDateString(),
-				order.customer?.name || '',
-				order.receivedBy?.name || '',
-				<a
-					key={`pdf-${order._id}`}
-					target='_blank'
-					href={`${airontoolsAPI}/basic-reports/repair-order/${order._id}`}
-					rel='noreferrer'
-				>
-					<PDFIcon />
-				</a>,
-				<button
-					key={`edit-${order._id}`}
-					className='table__button table__button--edit'
-					onClick={() => handleEditOrder(order._id)}
-				>
-					<EditIcon />
-				</button>,
-			])
-			.reverse(),
+		rows: orders.map(order => [
+			`AT${order.control}`,
+			new Date(order.createdAt).toLocaleDateString(),
+			order.customer?.name || '',
+			order.receivedBy?.name || '',
+			<a
+				key={`pdf-${order._id}`}
+				target='_blank'
+				href={`${airontoolsAPI}/basic-reports/repair-order/${order._id}`}
+				rel='noreferrer'
+			>
+				<PDFIcon />
+			</a>,
+			<button
+				key={`edit-${order._id}`}
+				className='table__button table__button--edit'
+				onClick={() => handleEditOrder(order._id)}
+			>
+				<EditIcon />
+			</button>,
+		]),
 	};
 
 	return (
