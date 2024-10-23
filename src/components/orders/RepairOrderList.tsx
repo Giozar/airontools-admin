77720f4312a17d1206bf74130cabd31a6fdbf092
Citlalli <citlalli688@outlook.com>
2024-10-23 09:31:53 -0600

    add: se añade el componente CircularCheckbox por estilo

--- src/components/orders/RepairOrderList.tsx
+++ src/components/orders/RepairOrderList.tsx
@@ -1,15 +1,14 @@
 // Importing necessary components, hooks, and libraries
 import DownloadButtons from '@components/commons/DownloadButtons';
 import TableComponent from '@components/commons/DynamicTable';
+import CircularCheckbox from '@components/commons/form/CircularCheckbox';
 import LimitInput from '@components/commons/Pagination/LimitInput';
 import Pagination from '@components/commons/Pagination/Pagination';
 import Searchbar from '@components/search/Searchbar';
-import EditIcon from '@components/svg/EditIcon';
 import PDFIcon from '@components/svg/PDFIcon';
 import { airontoolsAPI } from '@configs/api.config';
 import useDebounce from '@hooks/search/useDebounce';
-import { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import useOrders from './hooks/useOrders';
 
 // Main component for displaying repair orders list
@@ -21,20 +20,22 @@ export default function RepairOrderList() {
 		useOrders();
 	const { debouncedFetch } = useDebounce(fetchOrders, 300);
 
-	const navigate = useNavigate();
+	//const navigate = useNavigate();
 
 	useEffect(() => {
 		debouncedFetch(searchTerm);
 	}, [searchTerm, debouncedFetch]);
 
-	const handleEditOrder = useCallback(
+	/*const handleEditOrder = useCallback(
 		(orderId: string) => {
 			navigate(`editar-orden/${orderId}`);
 		},
 		[navigate],
-	);
+	);*/
+
 	// Maneja la selección/deselección de filas y checkboxes
 	const handleToggleCheck = (index: number) => {
+		console.log(index);
 		if (checkedRows.includes(index)) {
 			setCheckedRows(checkedRows.filter(i => i !== index));
 		} else {
@@ -49,7 +50,7 @@ export default function RepairOrderList() {
 			'Cliente',
 			'Recibido por',
 			'PDF',
-			'Editar Orden',
+			// 'Editar Orden', //Por petición del jefe
 			'',
 		],
 		rows: orders.map((order, index) => [
@@ -65,19 +66,11 @@ export default function RepairOrderList() {
 			>
 				<PDFIcon />
 			</a>,
-			<button
-				key={`edit-${order._id}`}
-				className='table__button table__button--edit'
-				onClick={() => handleEditOrder(order._id)}
-			>
-				<EditIcon />
-			</button>,
-			<input
+			<CircularCheckbox
 				key={`check-${order._id}`}
-				type='checkbox'
-				name='checkOrder'
-				checked={checkedRows.includes(index)} // Muestra si está seleccionado
-				onChange={() => handleToggleCheck(index)} // Cambia el estado al hacer clic
+				id={`check-${index}`}
+				checked={checkedRows.includes(index)}
+				onChange={() => handleToggleCheck(index)}
 			/>,
 		]),
 	};
